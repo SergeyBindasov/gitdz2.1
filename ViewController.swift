fd6acb810f4439fb5c9f56db21ee02fdcd252f7a
Sergey <bindasov.sergey@gmail.com>
2022-11-26 17:56:19 +0100

    текст из файла
    смотреть ViewController - textLabel.text

--- ViewController.swift
+++ ViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textLabel.numberOfLines = 0
+        textLabel.text = "Мы на столько крутые, что уже успели поработать со следующими команиями: \nООО «Рога и копыта»\nИздательство «Читый лист»\nКосмопорт «Черезтерновый Кзвёздный» \nДизайн-студия имени Слишком Известного Персонажа"
     }
 
 
